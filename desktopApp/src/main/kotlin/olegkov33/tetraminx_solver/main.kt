80f0f2559d7d08f494951a118eeee90ab8cbf741
OlegKov33 <olkova.oleg.kovalchuk@gmail.com>
2026-07-27 16:49:40 +0100

    created buttons menu

--- desktopApp/src/main/kotlin/olegkov33/tetraminx_solver/main.kt
+++ desktopApp/src/main/kotlin/olegkov33/tetraminx_solver/main.kt
@@ -2,12 +2,13 @@ package olegkov33.tetraminx_solver
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import olegkov33.solver.ColourButtons
 
-fun main() = application {
+fun main(): Unit = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "Tetraminx-solver",
     ) {
-        App()
+        ColourButtons()
     }
 }
---
+++ shared/src/commonMain/kotlin/olegkov33/solver/Buttons.kt
@@ -0,0 +1,64 @@
+package olegkov33.solver
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+/**
+ * Function used to create a row with predetermined buttons.
+ * Buttons are used to control Tetra-minx color per cell
+ */
+@Composable
+fun ColourButtons() {
+    Row(){
+        createDoubleColourButton(Color.Red, Color.LightGray, 80.dp)
+        createDoubleColourButton(Color.Green, Color.Magenta, 80.dp)
+        createDoubleColourButton(Color.Blue, Color.Cyan, 80.dp)
+        createDoubleColourButton(Color.Yellow, Color.DarkGray, 80.dp)
+    }
+}
+
+@Composable
+private fun createDoubleColourButton(topColor: Color,
+                                     bottomColor: Color,
+                                     buttonSize : Dp){
+
+    return Column(modifier = Modifier
+    .size(buttonSize)
+        .padding(10.dp)
+    .clip(RoundedCornerShape(70))) {
+        Box(
+            modifier = Modifier.background(
+                color = topColor,
+                )
+                .weight(1f)
+                .fillMaxHeight()
+                .fillMaxWidth()
+                .clickable{println(topColor)}
+        )
+        Box(
+            modifier = Modifier
+                .background(
+                    color = bottomColor,
+                )
+                .weight(1f)
+                .fillMaxHeight()
+                .fillMaxWidth()
+                .clickable{println(bottomColor)}
+        )
+    }
+}
+
