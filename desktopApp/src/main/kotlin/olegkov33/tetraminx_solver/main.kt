0a58379495f227abbaab08a44b8cf17ec49fb546
OlegKov33 <olkova.oleg.kovalchuk@gmail.com>
2026-07-28 11:47:31 +0100

    created tetra-minx flat 1D model

--- desktopApp/src/main/kotlin/olegkov33/tetraminx_solver/main.kt
+++ desktopApp/src/main/kotlin/olegkov33/tetraminx_solver/main.kt
@@ -2,13 +2,13 @@ package olegkov33.tetraminx_solver
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import olegkov33.solver.ColourButtons
+import olegkov33.solver.TetraminxModel
 
 fun main(): Unit = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "Tetraminx-solver",
     ) {
-        ColourButtons()
+        TetraminxModel().createTetraminx()
     }
 }
---
+++ shared/src/commonMain/kotlin/olegkov33/solver/TetraminxModel.kt
@@ -0,0 +1,158 @@
+package olegkov33.solver
+
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.PathHitTester
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.dp
+
+
+val innerArray = arrayOf(
+    arrayOf(0,0,0,0,0,0),
+    arrayOf(1,1,1,1,1,1),
+    arrayOf(2,2,2,2,2,2),
+    arrayOf(3,3,3,3,3,3)
+)
+
+class TetraminxModel {
+
+    /**
+     * Method creates a flat 1D tetra-minx, all sides 1 - 4 are laid out sequentially
+     */
+    @Composable
+    fun createTetraminx(){
+
+        Row(modifier = Modifier.width(400.dp).height(400.dp)) {
+            for( i in 0 .. 3){
+                Box(modifier = Modifier.weight(1f)){
+
+                    createFace(i)
+                }
+            }
+        }
+    }
+
+    /**
+     * Creates a 1D hexagonal face that occupies an entire space it was created in
+     * @param faceNum parameter that is used to indicate which face on tetra-minx is being created
+     */
+    @Composable
+    fun createFace(faceNum: Int) {
+        val pathList = remember { mutableListOf<Path>() }
+
+        return Canvas(modifier = Modifier
+            .fillMaxSize()
+            .pointerInput(Unit) {
+                detectTapGestures{pos ->
+                    pathList.forEachIndexed { index, path->
+
+                        // Path tester asks - is the click position (pos) inside any path (path)?
+                        if(PathHitTester(path).contains(pos)){
+                            println("what is $path and $pos?")
+
+                            println("pressed $index on face $faceNum")
+                        }
+                    }
+                }
+            }
+        ) {
+
+            pathList.clear()
+
+            pathList.add( createFirstEdge(size))
+            pathList.add(createSecondEdge(size))
+            pathList.add(createThirdEdge(size))
+            pathList.add(createFourthEdge(size))
+            pathList.add(createFifthEdge(size))
+            pathList.add(createSixthEdge(size))
+
+            for( i in pathList.indices){
+                if(i % 2 == 0){
+                    drawPath(pathList[i], Color.Black)
+                }
+                else{
+                    drawPath(pathList[i], Color.LightGray)
+                }
+            }
+
+        }
+    }
+
+
+    private fun createFirstEdge(size: Size) : Path{
+
+        return  Path().apply {
+                moveTo(size.width / 2f, size.height / 2f)
+                lineTo(size.width / 4f, 0f)
+                lineTo(0f, size.height / 2f)
+                close()
+            }
+
+    }
+
+    private fun createSecondEdge(size : Size) : Path {
+
+        return Path().apply {
+            moveTo(size.width / 2f, size.height / 2f)
+            lineTo(size.width / 4f, 0f)
+            lineTo(size.width / (4f / 3f), 0f)
+            close()
+        }
+
+    }
+
+    private fun createThirdEdge(size : Size) : Path{
+
+        return Path().apply {
+            moveTo(size.width / 2f, size.height / 2f)
+            lineTo(size.width / (4f / 3f), 0f)
+            lineTo(size.width, size.height / 2f)
+            close()
+        }
+    }
+
+    private fun createFourthEdge(size: Size) : Path {
+        return Path().apply {
+            moveTo(size.width / 2f, size.height / 2f)
+            lineTo(size.width / (4f / 3f), size.height)
+            lineTo(size.width, size.height / 2f)
+            close()
+        }
+    }
+
+    private fun createFifthEdge(size: Size) : Path {
+        return Path().apply {
+            moveTo(size.width / 2f, size.height / 2f)
+            lineTo(size.width / 4f, size.height)
+            lineTo(size.width / (4f / 3f), size.height)
+            close()
+        }
+    }
+
+    private fun createSixthEdge(size: Size) : Path {
+
+        return Path().apply {
+            moveTo(size.width / 2f, size.height / 2f)
+            lineTo(size.width / 4f, size.height)
+            lineTo(0f, size.height / 2f)
+            close()
+        }
+    }
+}
